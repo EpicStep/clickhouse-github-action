621e971a6e081c5896f58c8010afab51d07e5eb1
Stepan Rabotkin <epicstyt@gmail.com>
2024-10-06 20:34:56 +0300

    fix: use new image name

--- start-clickhouse.sh
+++ start-clickhouse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting ClickHouse"
-docker run --name clickhouse -p 9000:9000 -p 8123:8123 -d yandex/clickhouse-server
+docker run --name clickhouse -p 9000:9000 -p 8123:8123 -d clickhouse/clickhouse-server
